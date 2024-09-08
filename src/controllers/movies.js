9b2eab24be56b494cb6e8d9ab6ab376ff4841b41
Oleksandra <sashakirina@gmail.com>
2024-09-08 17:57:16 +0200

    add patch delete

--- src/controllers/movies.js
+++ src/controllers/movies.js
@@ -28,3 +28,58 @@ export const getMovieByIdController = async (req, res) => {
     data,
   });
 };
+
+export const addMovieController = (req, res) => {
+  const movie = movieServisces.createMovie(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: 'movie successfully added',
+    data: movie,
+  });
+};
+
+export const upsertMovieController = async (req, res) => {
+  const { movieId } = req.params;
+  const { isNew, data } = await movieServisces.updateMovie(
+    { _id: movieId },
+    req.body,
+    {
+      upsert: true,
+    },
+  );
+
+  const status = isNew ? 201 : 200;
+
+  res.json({
+    status,
+    message: 'Movie upsert succesfully',
+    data,
+  });
+};
+
+export const patchMovieController = async (req, res) => {
+  const { movieId } = req.params;
+  const data = await movieServisces.updateMovie({ _id: movieId }, req.body);
+
+  if (!data) {
+    throw createHttpError(404, `Movie with id ${movieId} not found`);
+  }
+
+  res.json({
+    status: 200,
+    message: 'Movie successfully patched',
+    data,
+  });
+};
+
+export const deleteMovieController = async (req, res) => {
+  const { movieId } = req.params;
+  const movie = await movieServisces.deleteMovie({ _id: movieId });
+
+  if (!movie) {
+    throw createHttpError(404, `Movie with id ${movieId} not found`);
+  }
+
+  res.status(204).send();
+};
--- src/routers/movies.js
+++ src/routers/movies.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 import {
+  addMovieController,
+  deleteMovieController,
   getMovieByIdController,
   getMoviesController,
+  patchMovieController,
+  upsertMovieController,
 } from '../controllers/movies.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
@@ -10,5 +14,13 @@ const moviesRouter = Router();
 moviesRouter.get('/', ctrlWrapper(getMoviesController));
 
 moviesRouter.get('/:movieId', ctrlWrapper(getMovieByIdController));
+
+moviesRouter.post('/', ctrlWrapper(addMovieController));
+
+moviesRouter.put('/:movieId', ctrlWrapper(upsertMovieController));
+
+moviesRouter.patch('/:movieId', ctrlWrapper(patchMovieController));
+
+moviesRouter.delete('/:movieId', ctrlWrapper(deleteMovieController));
 
 export default moviesRouter;
